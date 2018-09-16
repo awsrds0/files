8c934ad37c0cf0d1d75af3a0ca4e3c936e96095b
awsrds0 <41788322+awsrds0@users.noreply.github.com>
2018-09-16 19:38:50 +0400

    Update likemain.js

--- likemain.js
+++ likemain.js
@@ -1,4 +1,8 @@
-function likemain()
-{
-    alert('hello');
-}
+var script = document.createElement("script")
+script.type = "text/javascript";
+//Chrome,Firefox, Opera, Safari 3+
+script.onload = function(){
+console.log("Script is loaded");
+};
+//script.src = "file1.js";
+document.getElementsByTagName("head")[0].appendChild(script);
